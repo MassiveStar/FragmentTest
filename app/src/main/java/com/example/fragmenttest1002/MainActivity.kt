ebe986189053f3a1479ef3080133133f3e3ea5c6
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-02-10 10:56:05 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/fragmenttest1002/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fragmenttest1002
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/* App creada en fecha 10.02.2021 para practicar fragmentos */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
